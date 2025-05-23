93cf8d75b2f53605f89390592ed29eda4431896d
shivanshchoudharygit <shivanshchoudhary333@gmail.com>
2025-05-23 11:38:22 +0530

    2 way code change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, AboutComponent],
+  imports: [HeaderComponent,ProfileComponent, FooterComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-profile',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent {
+  username: string = '';
+}
